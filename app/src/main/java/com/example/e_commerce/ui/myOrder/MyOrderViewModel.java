165c447b733cfcc3d038ead6e41c2cf903abc683
yuvastriker <aryanjain85297@gmail.com>
2020-07-20 18:49:40 +0530

    HomeActivity

---
+++ app/src/main/java/com/example/e_commerce/ui/myOrder/MyOrderViewModel.java
@@ -0,0 +1,19 @@
+package com.example.e_commerce.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyOrderViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MyOrderViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/e_commerce/ui/myReward/MyRewardFragment.java
@@ -0,0 +1,38 @@
+package com.example.e_commerce.ui.myReward;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.e_commerce.R;
+
+public class MyReward extends Fragment {
+
+    private MyRewardViewModel mViewModel;
+
+    public static MyReward newInstance() {
+        return new MyReward();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_reward, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MyRewardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
